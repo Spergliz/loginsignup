84931cbccd254f4d181d8b8ab977264a4e601fcc
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-12-08 14:52:51 -0700

    g4re

--- js/main.js
+++ js/main.js
@@ -3,12 +3,17 @@
 // HTML VARIABLES
 let signInBtn = document.getElementById('sign-in-btn');
 let signUpBtn = document.getElementById('sign-up-btn');
+let UsernameE1 = document.getElementById('usersignup')
+let passwordE1 = document.getElementById('Userpassword')
+let passwordC = document.getElementById('passwordC')
+let usernameE2 = document.getElementById ('userlogin') 
+let passwordE2 = document.getElementById ('passlogin') 
 
 // SIGN UP BTN CLICKED
 signUpBtn.addEventListener('click', signUpHandler);
 
 function signUpHandler() {
-  console.log('Sign Up Btn Clicked');
+  Users.push(newuser(UsernameE1.value,passwordE1.value))
 }
 
 // SIGN IN BTN CLICKED
@@ -17,3 +22,11 @@ signInBtn.addEventListener('click', signInHandler);
 function signInHandler() {
   console.log('Sign In Btn Clicked');
 }
+
+function newuser(username,password) {
+  return {
+  user: username,
+  pass: password,
+  }
+}
+console.log(Users)
